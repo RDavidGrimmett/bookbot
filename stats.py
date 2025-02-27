87a760c7f9e979073a8bd5ef84a6de684ba9be34
Zenny33 <Zenny33@Extremetoaster.com>
2025-02-26 21:09:20 -0700

    testing sort code

--- stats.py
+++ stats.py
@@ -11,3 +11,7 @@ def get_num_character(book_text):
             else:
                 character_count[character] = 1
     return character_count
+
+def sort_on(character_count):
+    sorted_character_count = dict(sorted(character_count.items(), key=lambda item: item[1], reverse=True))
+    print(sorted_character_count)
