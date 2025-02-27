734745f04e49d95c62da3a4a18c27ef3be96cf8e
Zenny33 <Zenny33@Extremetoaster.com>
2025-02-26 19:19:23 -0700

    added bookbot word_count func

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    word_count = len(book_text.split())
+    print(f"{word_count} words found in the document")
+
+main()
