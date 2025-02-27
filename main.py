82551793299fad023af0fa2a701874df866cdae6
Zenny33 <Zenny33@Extremetoaster.com>
2025-02-26 20:05:32 -0700

    added new function stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_character
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,7 +7,7 @@ def get_book_text(path_to_file):
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
-    word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
-    
+    character_count = get_num_character(book_text)
+    print(character_count)
+
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(book_text):
     word_count = len(book_text.split())
     return word_count
+
+def get_num_character(book_text):
+    character_count = {}
+    for words in book_text.lower():
+        for character in words:
+            if character in character_count:
+                character_count[character] += 1
+            else:
+                character_count[character] = 1
+    return character_count
