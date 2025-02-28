b23b79cbefed5ae37e9d3631dfa351cb3d2e310b
Zenny33 <Zenny33@Extremetoaster.com>
2025-02-27 18:04:31 -0700

    began report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from stats import get_num_words
 from stats import get_num_character
+from stats import sort_on
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,7 +9,10 @@ def get_book_text(path_to_file):
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
+    word_count = get_num_words(book_text)
     character_count = get_num_character(book_text)
-    print(character_count)
+    sort_char = sort_on(character_count)
+    for key, value in sort_char.items():
+        print(f"{key}: {value}")
 
 main()
--- stats.py
+++ stats.py
@@ -6,12 +6,12 @@ def get_num_character(book_text):
     character_count = {}
     for words in book_text.lower():
         for character in words:
-            if character in character_count:
+            if character in character_count and character.isalpha():
                 character_count[character] += 1
             else:
                 character_count[character] = 1
     return character_count
 
 def sort_on(character_count):
-    sorted_character_count = dict(sorted(character_count.items(), key=lambda item: item[1], reverse=True))
-    print(sorted_character_count)
+    sort_char_count = dict(sorted(character_count.items(), key=lambda item: item[1], reverse=True))
+    return sort_char_count
