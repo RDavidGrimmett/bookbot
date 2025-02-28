1001d0f8ed49958713eaa5a1c15ff19298910e4b
Zenny33 <Zenny33@Extremetoaster.com>
2025-02-27 19:47:58 -0700

    file path works

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys 
 from stats import get_num_words
 from stats import get_num_character
 from stats import sort_on
+
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,12 +11,12 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    book_text = get_book_text(path_to_file)
     word_count = get_num_words(book_text)
     character_count = get_num_character(book_text)
     sort_char = sort_on(character_count)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -21,5 +24,15 @@ def main():
         if key.isalpha():
             print(f"{key}: {value}")
     print("============= END ===============")
+
+path_to_file = sys.argv[1]
+
+if len(sys.argv) != 2:
+    print ("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    get_book_text(path_to_file)
+
+
 
 main()
